982a8a0ec6aa4c5649d75af5a21c4b1aeb1bd3cc
Guilherme Lima <grguilh2rm2lima@gmail.com>
2026-01-08 20:48:11 -0300

    feat: - Regra de negócio para verificar se uma sala já está reservada.

--- src/main/java/com/guilhermecode/reserva_salas_api/repositories/ReservaRepository.java
+++ src/main/java/com/guilhermecode/reserva_salas_api/repositories/ReservaRepository.java
@@ -2,5 +2,29 @@ package com.guilhermecode.reserva_salas_api.repositories;
 
 import com.guilhermecode.reserva_salas_api.models.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ReservaRepository extends JpaRepository<Reserva, Long> { }
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    @Query("""
+                SELECT COUNT(r) > 0
+                FROM Reserva r
+                WHERE r.sala.id = :salaId
+                  AND r.dataReserva = :data
+                  AND (
+                        :horaInicio < r.horaFim
+                    AND :horaFim > r.horaInicio
+                  )
+            """)
+    boolean isReserved(
+            @Param("salaId") Long salaId,
+            @Param("data") LocalDate data,
+            @Param("horaInicio") LocalTime horaInicio,
+            @Param("horaFim") LocalTime horaFim
+    );
+
+}
--- src/main/java/com/guilhermecode/reserva_salas_api/services/ReservaService.java
+++ src/main/java/com/guilhermecode/reserva_salas_api/services/ReservaService.java
@@ -1,9 +1,12 @@
 package com.guilhermecode.reserva_salas_api.services;
 
 import com.guilhermecode.reserva_salas_api.models.Reserva;
+import com.guilhermecode.reserva_salas_api.models.Sala;
 import com.guilhermecode.reserva_salas_api.repositories.ReservaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,11 +16,22 @@ public class ReservaService {
     @Autowired
     private ReservaRepository reservaRepository;
 
-    public List<Reserva> getAll(){
+    public List<Reserva> getAll() {
         return reservaRepository.findAll();
     }
 
-    public Long create(Reserva reserva){
+    public Long create(Reserva reserva) {
+        boolean isReserved = reservaRepository.isReserved(
+                reserva.getSala().getId(),
+                reserva.getDataReserva(),
+                reserva.getHoraInicio(),
+                reserva.getHoraFim()
+        );
+        if (isReserved) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Horário da sala já está reservado.");
+        }
         return reservaRepository.save(reserva).getId();
     }
+
+
 }
